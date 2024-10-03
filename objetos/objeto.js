28486a04948a6e402bd89a86818ad821c29b77d6
maickribeiro <maickribeiro93@gmail.com>
2024-10-02 22:35:40 -0300

    Create objeto.js

---
+++ objetos/objeto.js
@@ -0,0 +1,23 @@
+const pessoa = {
+    nome: "Maick"
+}
+
+pessoa.sobrenome = "Ribeiro"
+console.log(pessoa)
+
+const pessoa2 = new Object()
+pessoa2.nome = "Laís"
+console.log(pessoa2)
+
+const nome = new String("Maick")
+console.log(nome)
+
+const boleano = new Boolean("false")
+console.log(boleano)
+
+const data = new Date()
+console.log(data)
+
+console.log(Date())
+
+ 
